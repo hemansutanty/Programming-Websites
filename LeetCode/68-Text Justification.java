7fe5e09638550c4dea66623c77041b79c8978c78
Hemansu Tanty <hemansu.tanty@gmail.com>
2021-06-03 19:30:34 +0530

    Added codes for 3rd June, 2021

---
+++ LeetCode/68-Text Justification.java
@@ -0,0 +1,116 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Given an array of words and a width maxWidth, format the text such that each line has exactly maxWidth characters and is fully (left and right) justified.
+
+You should pack your words in a greedy approach; that is, pack as many words as you can in each line. Pad extra spaces ' ' when necessary so that each line has exactly maxWidth characters.
+
+Extra spaces between words should be distributed as evenly as possible. If the number of spaces on a line do not divide evenly between words, the empty slots on the left will be assigned more spaces than the slots on the right.
+
+For the last line of text, it should be left justified and no extra space is inserted between words.
+
+Note:
+
+A word is defined as a character sequence consisting of non-space characters only.
+Each word's length is guaranteed to be greater than 0 and not exceed maxWidth.
+The input array words contains at least one word.
+ 
+
+Example 1:
+
+Input: words = ["This", "is", "an", "example", "of", "text", "justification."], maxWidth = 16
+Output:
+[
+   "This    is    an",
+   "example  of text",
+   "justification.  "
+]
+Example 2:
+
+Input: words = ["What","must","be","acknowledgment","shall","be"], maxWidth = 16
+Output:
+[
+  "What   must   be",
+  "acknowledgment  ",
+  "shall be        "
+]
+Explanation: Note that the last line is "shall be    " instead of "shall     be", because the last line must be left-justified instead of fully-justified.
+Note that the second line is also left-justified becase it contains only one word.
+Example 3:
+
+Input: words = ["Science","is","what","we","understand","well","enough","to","explain","to","a","computer.","Art","is","everything","else","we","do"], maxWidth = 20
+Output:
+[
+  "Science  is  what we",
+  "understand      well",
+  "enough to explain to",
+  "a  computer.  Art is",
+  "everything  else  we",
+  "do                  "
+]
+ 
+
+Constraints:
+
+1 <= words.length <= 300
+1 <= words[i].length <= 20
+words[i] consists of only English letters and symbols.
+1 <= maxWidth <= 100
+words[i].length <= maxWidth*/
+
+class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        int left = 0;
+        List<String> result  = new ArrayList<>();
+        while(left<words.length){
+            // find the right most word we can accomodate in the line
+            int right = findRight(left, words, maxWidth);
+            // justify / pad lines with range of left and right words
+            result.add(justify(left, right, words, maxWidth));
+            //Strat from th enext word onwards
+            left = right + 1;
+        }
+        return result;
+    }
+    //Function to find the rightmost word in the word list which can be accomodated in a line
+    public int findRight(int left, String[] words, int maxWidth){
+        int right = left;
+        int currentLength = words[right].length();
+        right ++ ;
+        while(right < words.length && (currentLength+1+words[right].length()<=maxWidth)){
+            currentLength = currentLength+1+words[right].length();
+            right++;
+        }
+        return right - 1;
+    }
+    
+    public String justify(int left, int right, String[] words, int maxWidth){
+        if(right==left) return padResult(words[left], maxWidth);
+        boolean isLastLine = right==words.length-1;
+        int numSpaces = right-left;
+        int totalSpace = maxWidth-wordsLength(left, right, words);
+        
+        String space = isLastLine?" ":blank(totalSpace/numSpaces);
+        int remainder = isLastLine?0:totalSpace%numSpaces;
+        
+        StringBuilder result = new StringBuilder();
+        for(int i =left;i<=right;i++){
+            result.append(words[i]).append(space).append(remainder-- > 0?" ":"");
+        }
+        return padResult(result.toString().trim(), maxWidth);
+    }
+    public int wordsLength(int left, int right, String[] words){
+        int wordsLength = 0;
+        for(int i=left;i<=right;i++){
+            wordsLength+=words[i].length();
+        }
+        return wordsLength;
+    }
+    public String padResult(String result, int maxWidth){
+        return result + blank(maxWidth-result.length());
+    }
+    public String blank(int length){
+        return new String(new char[length]).replace('\0', ' ');
+    }
+}
